f81eb040b639d72da16d15c047207661de549d05
laura <laura@192.168.1.130>
2025-03-04 20:23:19 +0100

    version6

--- src/inventario/Inventario.java
+++ src/inventario/Inventario.java
@@ -2,31 +2,52 @@ package inventario;
 
 import java.util.ArrayList;
 
+/**
+ *
+ * @author Laura Vacas
+ * @version 1.0.0
+ * @since 2025/03/04
+ */
 public class Inventario {
+
     public ArrayList<Producto> listaProductos;
 
     public Inventario() {
         listaProductos = new ArrayList<>();
     }
 
+    /**
+     * Agrega productos a la lista de productos (ArrayList)
+     *
+     * @param p el producto a agregar a la lista
+     */
     public void agregar(Producto p) {
         listaProductos.add(p);
     }
 
+    /**
+     * Método para mostrar la informacion de cada uno de los productos
+     * 
+     * @throws IndexOutOfBoundsException 
+     */
     public void mostrar() {
         for (int i = 0; i < listaProductos.size(); i++) {
             listaProductos.get(i).mostrarInformacion();
         }
     }
 
+    /**
+     * Metodo main
+     * @param args argumentos
+     */
     public static void main(String[] args) {
         Inventario inventario = new Inventario();
         Producto p1 = Producto.crearProducto("Laptop", 1200.99, 10);
         Producto p2 = Producto.crearProducto("Mouse", 25.50, 50);
-        
+
         inventario.agregar(p1);
         inventario.agregar(p2);
-        
+
         System.out.println("Inventario de productos:");
         inventario.mostrar();
 
--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -1,47 +1,94 @@
 package inventario;
 
+/**
+ * @author Laura Vacas
+ * @version 1.0.0
+ * @since 2025/03/04
+ */
 public class Producto {
+
     private String nombre;
     private double precio;
     private int stock;
 
+    /**
+     * Método de fábrica para crear un nuevo Producto
+     *
+     * @param nombre El nombre del producto
+     * @param precio El precio del producto
+     * @param stock El stock del producto
+     * @return el nuevo producto creado
+     */
     public static Producto crearProducto(String nombre, double precio, int stock) {
-        
+
         Producto p = new Producto();
-        
+
         p.nombre = nombre;
-        
+
         p.precio = precio;
-        
+
         p.stock = stock;
-        
+
         return p;
     }
 
+    /**
+     * Obtiene el nombre del producto
+     *
+     * @return el nombre del producto
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Cambia el nombre del producto
+     *
+     * @param nombre nuevo valor que se le da a nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el precio del producto
+     *
+     * @return el precio del producto
+     */
     public double getPrecio() {
         return precio;
     }
 
+    /**
+     * Cambia el precio del producto
+     *
+     * @param precio nuevo valor que se le da a precio
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
+    /**
+     * Obtiene el stock del producto
+     *
+     * @return el stock del producto
+     */
     public int getStock() {
         return stock;
     }
 
+    /**
+     * Cambia el stock del producto
+     *
+     * @param stock nuevo valor que se le da al stock
+     */
     public void setStock(int stock) {
         this.stock = stock;
     }
 
+    /**
+     * Imprime por pantalla la información del producto
+     */
     public void mostrarInformacion() {
         System.out.println("Producto: " + nombre + " | Precio: " + precio + " | Stock: " + stock);
     }
